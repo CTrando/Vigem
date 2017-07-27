75915f72ce1b66fb6649a588627f4e1193e42036
CTrando <cameron.trando@yahoo.com>
2017-07-26 21:58:06 -0700

    Learning networking with node js and socket.io, hopefully will continue smoothly

--- core/src/com/ct/game/Vigem.java
+++ core/src/com/ct/game/Vigem.java
@@ -5,16 +5,25 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.ct.game.view.GameScreen;
+import io.socket.client.*;
+import io.socket.emitter.Emitter;
+import org.json.*;
+
+import java.net.URISyntaxException;
 
 public class Vigem extends Game {
 	private SpriteBatch batch;
 	private GameScreen gameScreen;
+	private Socket socket;
+	private String uuid;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		gameScreen = new GameScreen(this);
 		setScreen(gameScreen);
+		createConnection();
+		getSocketInfo();
 	}
 
 	@Override
@@ -29,5 +38,29 @@ public class Vigem extends Game {
 
 	public SpriteBatch getBatch() {
 		return batch;
+	}
+
+	private void createConnection() {
+		try{
+			socket = IO.socket("http://localhost:3000");
+			socket.connect();
+		} catch (URISyntaxException e) {
+			System.out.println(e);
+		}
+	}
+
+	private void getSocketInfo() {
+		socket.on("socketID", new Emitter.Listener() {
+			@Override
+			public void call(Object... args) {
+				JSONObject data = (JSONObject) args[0];
+				try {
+					uuid = data.getString("id");
+					System.out.println("Your unique user id is " + uuid);
+				} catch (JSONException e) {
+					System.out.println(e);
+				}
+			}
+		});
 	}
 }
